41976b2a749ddfd408a749f1680581d2062d6054
VasuG19 <99899682+VasuG19@users.noreply.github.com>
2023-01-10 14:55:22 +0000

    authentication

--- src/authenticate.php
+++ src/authenticate.php
@@ -2,6 +2,13 @@
 class Authenticate extends Endpoint
 {
   public function __construct() {
+        $db = new Database("db/chiplay.sqlite");
+        $this->validateRequestMethod("POST");
+        $this->validateAuthParameters();
+        $this->initialiseSQL();
+        $queryResult = $db->executeSQL($this->getSQL(), $this->getSQLParams());
+        $this->validateUsername($queryResult);
+        $this->validatePassword($queryResult); 
 
         http_response_code(503);
 
@@ -10,6 +17,46 @@ class Authenticate extends Endpoint
           "message" => "endpoint under construction",
           "data" => null
         ));
+    }
+
+    private function validateRequestMethod($method) {
+        if ($_SERVER['REQUEST_METHOD'] != $method){
+            die( json_encode( array(
+                "message" => "invalid request method"
+            )));
+        }
+    }
+
+     private function validateAuthParameters() {
+        if ( !isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) ) {
+            die( json_encode( array(
+                "message" => "username and password required"
+            )));
+        }
+    }
+
+    private function validateUsername($data) {
+        if (count($data)<1) {
+            die( json_encode( array(
+                "message" => "invalid credentials"
+            )));
+        }
+    }
+
+    private function validatePassword($data) {
+        if (!password_verify($_SERVER['PHP_AUTH_PW'], $data[0]['password'])) {
+            die( json_encode( array(
+                "message" => "invalid credentials"
+            )));
+        }
+    }
+
+    protected function initialiseSQL() {
+        $sql = "SELECT *
+                FROM account
+                WHERE account.username = :username";
+        $this->setSQL($sql);
+    $this->setSQLParams(['username' => $_SERVER['PHP_AUTH_USER']]);
     }
 
 
